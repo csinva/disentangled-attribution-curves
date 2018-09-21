539f0ac546d2a379f227da915ad5ddde46f4e3a1
Summer Devlin <summer@Summers-MBP.hsd1.ca.comcast.net>
2018-09-20 21:50:31 -0700

    interference variables added

--- data.py
+++ data.py
@@ -3,3 +3,20 @@ X = [[1, 1], [1, -1], [-1, 1], [-1, -1]]
 Y = [[0], [1], [1], [0]]
 X = np.array(X)
 Y = np.array(Y)
+
+"""
+PARAMETERS
+n: number of data points
+continuous_x: true if x takes continuous values, false otherwise
+nonsense_vars: adds the specified additional features that have no effect on the output
+OUTPUT
+a tuple containing at index 0 a matrix with rows representing datapoints X,
+and at index 1 a column vector of results Y
+"""
+def generate_xor_data(n, continuous_x=False, nonsense_vars=0):
+    if(not continuous_x):
+        X = np.random.choice([-1, 1], (n, 2 + nonsense_vars))
+    else:
+        X = np.random.uniform(-1, 1, (n, 2 + nonsense_vars))
+    Y = np.transpose(X[:, 0] * X[:, 1] < 0)
+    return (X, Y)
--- interactions.py
+++ interactions.py
@@ -2,24 +2,24 @@ import numpy as np
 from sklearn import tree
 
 from model_train import train
-from data import X, Y
+from data import generate_xor_data
 
 
 """
 PARAMETERS
 model: a decision tree trained on some data set
 outcome_space_x: a matrix describing the possible x values in the total outcome space
-outcome_space_y: a matrix describing the possible y values in the total outcome space
+input_space_y: a matrix describing the possible y values in the total outcome space
 these two should be ordered such that x outcomes match with y outcomes
 assignment: an assignment vector starting at x_0
 S: a vector with boolean values indicating variables whose interactions we are attempting to describe,
 ordered starting at x_0"""
 """
 OUTPUT
-a matrix with row vectors in the form [y_i, p(y_i)] for y_i unique y values in outcome_space_y, and
+a matrix with row vectors in the form [y_i, p(y_i)] for y_i unique y values in input_space_y, and
 p(y_i) a probability value associated with y_i
 """
-def interactions(model, outcome_space_x, outcome_space_y, assignment, S):
+def interactions(model, outcome_space_x, input_space_y, assignment, S):
     features = model.tree_.feature
     thresholds = model.tree_.threshold
     path = model.decision_path(assignment).indices
@@ -31,7 +31,7 @@ def interactions(model, outcome_space_x, outcome_space_y, assignment, S):
             geq = assignment[0][feature_ind] >= threshold
             decision_rules.append((feature_ind, threshold, geq))
     outcomes = []
-    for i in range(len(outcome_space_y)):
+    for i in range(len(input_space_y)):
         complies = True
         for d in decision_rules:
             if(d[2]):
@@ -39,21 +39,22 @@ def interactions(model, outcome_space_x, outcome_space_y, assignment, S):
             else:
                 complies = complies and outcome_space_x[i][d[0]] < d[1]
         if complies:
-            outcomes.append(outcome_space_y[i])
+            outcomes.append(input_space_y[i])
     unique, counts = np.unique(outcomes, return_counts = True)
     unique = np.reshape(unique, (-1, 1))
-    probs = np.reshape(counts/len(unique), (-1, 1))
+    probs = np.reshape(counts/len(outcomes), (-1, 1))
     return np.hstack((unique, probs))
 
 def main():
+    X, Y = generate_xor_data(500, nonsense_vars=1)
     model = train(X, Y)
-    X_test = [[1, 1]]
-    print(interactions(model, X, Y, X_test, [1, 0]))
-    print(interactions(model, X, Y, X_test, [0, 1]))
-    print(interactions(model, X, Y, X_test, [1, 1]))
-    X_test = [[1, -1]]
-    print(interactions(model, X, Y, X_test, [1, 0]))
-    print(interactions(model, X, Y, X_test, [0, 1]))
-    print(interactions(model, X, Y, X_test, [1, 1]))
+    X_tests = [[[1, 1, -1]], [[1, -1, -1]], [[1, 1, 1]], [[-1, 1, 1]]]
+    for a in X_tests:
+        print("Assignment:", a)
+        for i in range(5):
+            s = np.random.choice([1, 0], (3))
+            print("interactions for variables:", s)
+            print(interactions(model, X, Y, a, s))
+
 
 main()
