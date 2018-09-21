ad19ab863d1eaf090a51160657218bc436e057cb
Summer Devlin <summer@Summers-MacBook-Pro.local>
2018-09-21 11:31:34 -0700

    tentative continuous x

--- interactions.py
+++ interactions.py
@@ -4,6 +4,35 @@ from sklearn import tree
 from model_train import train
 from data import generate_xor_data
 
+"""
+define an INTERVAL as a tuple, where the entry at index 0 is the lower bound, and the entry at index 1
+is the upper bound, both exclusive.
+"""
+def interval_intersect(a, b):
+    radius_a = (a[1] - a[0])/2
+    radius_b = (b[1] - b[0])/2
+    centroid_a = radius_a + a[0]
+    centroid_b = radius_b + b[0]
+    return np.abs(centroid_b - centroid_a) < radius_a + radius_b
+
+def join_intervals(a, b):
+    if(not interval_intersect(a, b)):
+        return -1
+    return (max(a[0], b[0]), min(a[1], b[1]))
+
+def test_intervals():
+    i1 = (0, 3)
+    i2 = (2, 4)
+    i3 = (3, 10)
+    i4 = (-1, 2)
+    print(i1, "intersects with", i2, "?", interval_intersect(i1, i2))
+    print(i1, "intersects with", i3, "?", interval_intersect(i1, i3))
+    print(i3, "intersects with", i4, "?", interval_intersect(i3, i4))
+    print(i3, "intersects with", i2, "?", interval_intersect(i3, i2))
+    print(i4, "intersects with", i1, "?", interval_intersect(i4, i1))
+    print("joining", i1, "and", i2, ":", join_intervals(i1, i2))
+    print("joining", i3, "and", i2, ":", join_intervals(i3, i2))
+    print("joining", i1, "and", i4, ":", join_intervals(i1, i4))
 
 """
 PARAMETERS
@@ -45,7 +74,41 @@ def interactions(model, outcome_space_x, input_space_y, assignment, S):
     probs = np.reshape(counts/len(outcomes), (-1, 1))
     return np.hstack((unique, probs))
 
-def main():
+def interactions_continuous(model, outcome_space_x, input_space_y, assignment, S):
+    features = model.tree_.feature
+    thresholds = model.tree_.threshold
+    path = model.decision_path(assignment).indices
+    decision_rules = {} #store decision rules in dictionary: feature => valid interval
+    for i in path:
+        feature_ind = features[i]
+        if S[feature_ind] > 0 and feature_ind >= 0:
+            threshold = thresholds[i]
+            leq = assignment[0][feature_ind] < threshold
+            bound = (-1 ** leq) * float('inf')
+            interval = (min(threshold, bound), max(threshold, bound))
+            if(decision_rules.get(feature_ind) != None):
+                decision_rules[feature_ind] = interval_intersect(decision_rules.get(feature_ind), interval)
+            else:
+                decision_rules[feature_ind] = interval
+    outcomes = []
+    features = list(decision_rules.keys())
+    rules = list(decision_rules.values())
+    for i in range(len(input_space_y)):
+        complies = True
+        for j in range(len(features)):
+            feature = features[j]
+            rule = rules[j]
+            coord = outcome_space_x[i][feature]
+            complies = complies and interval_contains((coord, coord), rule)
+        if complies:
+            outcomes.append(input_space_y[i])
+    unique, counts = np.unique(outcomes, return_counts = True)
+    unique = np.reshape(unique, (-1, 1))
+    probs = np.reshape(counts/len(outcomes), (-1, 1))
+    return np.hstack((unique, probs))
+
+
+def test_nonsense_vars():
     X, Y = generate_xor_data(500, nonsense_vars=1)
     model = train(X, Y)
     X_tests = [[[1, 1, -1]], [[1, -1, -1]], [[1, 1, 1]], [[-1, 1, 1]]]
@@ -56,5 +119,16 @@ def main():
             print("interactions for variables:", s)
             print(interactions(model, X, Y, a, s))
 
+def test_continuous():
+    X, Y = generate_xor_data(10000, continuous_x=True)
+    model = train(X, Y)
+    X_tests = [[[-.5, -.5]], [[.5, .5]], [[-.5, .5]]]
+    S = [[1, 0], [0, 1], [0, 0], [1, 1]]
+    for a in X_tests:
+        print("Assignment:", a)
+        for s in S:
+            print("interactions for variables:", s)
+            print(interactions(model, X, Y, a, s))
 
-main()
+
+test_continuous()
