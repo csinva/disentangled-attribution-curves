910d1b18b9ba5a29ba4739de95388fa3137441c1
Summer Devlin <summer@Summers-MBP.hsd1.ca.comcast.net>
2018-11-27 22:52:18 -0800

    better heatmaps

--- interactions.py
+++ interactions.py
@@ -96,6 +96,90 @@ def fix_shape(distribution, unique_Y):
                 dist[j, 1] = distribution[i, 1]
     return dist
 
+def apply_rule(X, Y, threshold, feature, direction):
+    mask = []
+    if(direction == "geq"):
+        mask = X[:, feature] >= threshold
+    else:
+        mask = X[:, feature] < threshold
+    new_X = X[mask]
+    new_Y = Y[mask]
+    return (new_X, new_Y)
+
+def traverse_all_paths(model, input_space_x, outcome_space_y, S, continuous_y = False):
+    print("number of nodes", model.tree_.node_count)
+    children_left = model.tree_.children_left
+    children_right = model.tree_.children_right
+    feature = model.tree_.feature
+    threshold = model.tree_.threshold
+    datasets = {}
+    num_feats = np.count_nonzero(S)
+    datasets[0] = (input_space_x, outcome_space_y)
+    intervals = {}
+    intervals[0] =[(- float('inf'), float('inf'))] * num_feats
+    leaves = []
+    fringe = [0]
+    while len(fringe) > 0:
+        curr_node = fringe.pop()
+        #print("curr_node", curr_node)
+        bound = threshold[curr_node]
+        feat = feature[curr_node]
+        #print("bound", bound, "on feature", feat)
+        child_left = children_left[curr_node]
+        child_right = children_right[curr_node]
+        #print("children (left, right)", child_left, child_right)
+        if (child_left != child_right):
+            #print("non-leaf")
+            X, Y = datasets[curr_node]
+            #print("current node data set X\n", X)
+            #print("current node data set Y\n", Y)
+            left_data = (X, Y)
+            right_data = (X, Y)
+            left_interval = intervals[curr_node]
+            right_interval = intervals[curr_node]
+            #print("current node interval", intervals[curr_node])
+            if(S[feat] == 1):
+                left_data = apply_rule(X, Y, bound, feat, "less")
+                #print("left data", left_data)
+                right_data = apply_rule(X, Y, bound, feat, "geq")
+                #print("right data", right_data)
+                left_interval = intervals[curr_node][0:feat] + [(intervals[curr_node][feat][0], bound)] + intervals[curr_node][feat + 1:len(intervals[curr_node])]
+                #print("left interval", left_interval)
+                right_interval = intervals[curr_node][0:feat] + [(bound, intervals[curr_node][feat][1])] + intervals[curr_node][feat + 1:len(intervals[curr_node])]
+                #print("right interval", right_interval)
+            datasets[child_left] = left_data
+            datasets[child_right] = right_data
+            intervals[child_left] = left_interval
+            intervals[child_right] = right_interval
+            fringe.append(child_left)
+            fringe.append(child_right)
+        else:
+            #print("leaf")
+            leaves.append(curr_node)
+    values = []
+    #print("leaves", leaves)
+    for leaf in leaves:
+        X, Y = datasets[leaf]
+        inter = intervals[leaf]
+        proportion = np.count_nonzero(Y == 1)/Y.shape[0]
+        values.append(inter + [proportion])
+    return values
+
+def make_grid(values, interval_x, interval_y, di):
+    xdim = (int)((interval_x[1] - interval_x[0])/di)
+    ydim = (int)((interval_y[1] - interval_y[0])/di)
+    grid = np.zeros((ydim, xdim))
+    counts = np.zeros((ydim, xdim))
+    for i in range(ydim):
+        for j in range(xdim):
+            x_coord = j * di + interval_x[0]
+            y_coord = i * di + interval_y[0]
+            for v in values:
+                x_inter, y_inter, val = v
+                if x_coord>=x_inter[0] and x_coord < x_inter[1] and y_coord >= y_inter[0] and y_coord < y_inter[1]:
+                    grid[i, j] += val
+                    counts[i, j] += 1
+    return grid/counts
 
 def recover_intervals(model, num_features):
     split_feats = model.tree_.feature
@@ -171,48 +255,6 @@ def test_plot_many_trees3(n):
     print("total time", time.clock() - t_start)
     plot_2D_classifier(x_vals, distribution)
 
-
-def test_plot_many_trees2(n):
-    X, Y = generate_single_variable_boundary(100, (-20, 20), 0)
-    rf = train_rf(X, Y, n)
-    trees = rf.estimators_
-    t = time.clock()
-    boundaries = []
-    inputs = []
-    for model in trees:
-        intervals = recover_intervals(model, 1)
-        ins = generate_all_inputs(intervals, 1)
-        boundaries += intervals[0]
-        inputs += ins[0]
-    boundaries.sort()
-    inputs.sort()
-    print("boundaries time", time.clock() - t)
-    values = []
-    weights = [1/n] * n
-    t_avg = 0
-    for assignment in inputs:
-        t_start = time.clock()
-        v = aggregate_trees(trees, weights, X, Y, [[assignment]], [1])[1, 1]
-        t_end = time.clock()
-        t_avg += t_end - t_start
-        values.append(v)
-    print("average time", t_avg/len(inputs), "repetitions", len(inputs))
-    print("trees time", time.clock() - t)
-    print("values", values)
-    x_axis = np.arange(-20, 20, .1)
-    distribution = []
-    i = 0
-    boundaries = [- float('inf')] + boundaries + [float('inf')]
-    for x in x_axis:
-        if x >= boundaries[i] and x < boundaries[i + 1]:
-            distribution.append(values[i])
-        else:
-            i += 1
-            distribution.append(values[i])
-    print("total time", time.clock() - t)
-    plot_2D_classifier(x_axis, distribution)
-
-
 def test_nonsense_vars():
     X, Y = generate_xor_data(500, nonsense_vars=1)
     model = train(X, Y)
@@ -248,28 +290,77 @@ def test_continuous_y():
     plt.xlabel("input X")
     plt.show()
 
-def heat_map():
-    X, Y = generate_xor_data(10000, continuous_x=True)
+def heat_map_xor():
+    X, Y = generate_xor_data(500, continuous_x=True)
+    plt.scatter(x=X[:, 0], y=X[:, 1], c=Y)
+    plt.show()
     model = train(X, Y)
-    intervals = recover_intervals(model, 2)
-    inputs = generate_all_inputs(intervals)
-    heat_values = []
-    for x_1 in inputs[0]:
-        heat_row = []
-        for x_2 in inputs[1]:
-            heat_row.append(interactions_continuous(model, X, Y, [[x_1, x_2]], [1, 1])[1, 1])
-        heat_values.append(heat_row)
-    print(heat_values)
-    fig, ax = plt.subplots(figsize=(7, 7))
-    im = ax.imshow(heat_values)
+    x_axis = np.arange(-1, 1, .1)
+    y_axis = np.arange(-1, 1, .1)
+    dist = np.zeros((y_axis.shape[0], x_axis.shape[0]))
+    for i in range(y_axis.shape[0]):
+        for j in range(x_axis.shape[0]):
+            input = [[y_axis[i], x_axis[j]]]
+            vals = interactions_continuous(model, X, Y, input, [1, 1])
+            dist[i, j] = vals[1, 1]
+    fig, ax = plt.subplots()
+    im = ax.imshow(dist)
+    cbar_kw = {}
+    cbarlabel = "class +1 proportion"
+    cbar = ax.figure.colorbar(im, ax=ax, **cbar_kw)
+    cbar.ax.set_ylabel(cbarlabel, rotation=-90, va="bottom")
+    ax.set_xticks(np.arange(20, step=2))
+    ax.set_yticks(np.arange(20, step=2))
+    ax.set_xticklabels(np.round(np.arange(-1, 1, step = .2), 2))
+    ax.set_yticklabels(np.round(np.arange(-1, 1, step = .2), 2))
+    plt.show()
 
-    ax.set_xticks(np.arange(len(inputs[0])))
-    ax.set_yticks(np.arange(len(inputs[1])))
-    intervals_1_labels = [str((round(interval[0], 3), round(interval[1], 3))) for interval in intervals[0]]
-    intervals_2_labels = [str((round(interval[0], 3), round(interval[1], 3))) for interval in intervals[1]]
-    ax.set_xticklabels(intervals_1_labels)
-    ax.set_yticklabels(intervals_2_labels)
-    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",rotation_mode="anchor")
+def tree_dec(a):
+    x1, x2 = a
+    if x2 > 0:
+        if x1 > -1:
+            return 1
+        return -1
+    else:
+        if x1 > 2:
+            return 1
+        return -1
+def heat_map2():
+    X, Y = generate_x_y_data(1000, (-5, 5), tree_dec, features=2)
+    plt.scatter(x=X[:, 0], y=X[:, 1], c=Y)
+    plt.show()
+    model = train(X, Y)
+    values = traverse_all_paths(model, X, Y, [1, 1])
+    grid = make_grid(values, (-5, 5), (-5, 5), .1)
+    fig, ax = plt.subplots()
+    im = ax.imshow(grid)
+    cbar_kw = {}
+    cbarlabel = "class +1 proportion"
+    cbar = ax.figure.colorbar(im, ax=ax, **cbar_kw)
+    cbar.ax.set_ylabel(cbarlabel, rotation=-90, va="bottom")
+    ax.set_xticks(np.arange(100, step=10))
+    ax.set_yticks(np.arange(100, step=10))
+    ax.set_xticklabels(np.round(np.arange(-5, 5, step = 1), 2))
+    ax.set_yticklabels(np.round(np.arange(-5, 5, step = 1), 2))
+    plt.show()
+    plt.show()
+
+def heat_map_noise():
+    X, Y = generate_xor_data(500, continuous_x=True, nonsense_vars=5)
+    model = train(X, Y)
+    values = traverse_all_paths(model, X, Y, [1, 1, 0, 0, 0, 0, 0])
+    grid = make_grid(values, (-1, 1), (-1, 1), .01)
+    fig, ax = plt.subplots()
+    im = ax.imshow(grid)
+    cbar_kw = {}
+    cbarlabel = "class +1 proportion"
+    cbar = ax.figure.colorbar(im, ax=ax, **cbar_kw)
+    cbar.ax.set_ylabel(cbarlabel, rotation=-90, va="bottom")
+    ax.set_xticks(np.arange(200, step=20))
+    ax.set_yticks(np.arange(200, step=20))
+    ax.set_xticklabels(np.round(np.arange(-1, 1, step = .2), 2))
+    ax.set_yticklabels(np.round(np.arange(-1, 1, step = .2), 2))
+    plt.show()
     plt.show()
 
 def test_interactions_rf(n):
@@ -284,5 +375,20 @@ def test_interactions_rf(n):
     print("comp time", time.clock() - t)
     print(dist)
 
-test_plot_many_trees3(10000)
-#test_interactions_rf(10000)
+def test_new_heatmap():
+    X, Y = generate_xor_data(100, continuous_x = True)
+    model = train(X, Y)
+    values = traverse_all_paths(model, X, Y, [1, 1])
+    grid = make_grid(values, (-1, 1), (-1, 1), .01)
+    fig, ax = plt.subplots()
+    im = ax.imshow(grid)
+    cbar_kw = {}
+    cbarlabel = "class +1 proportion"
+    cbar = ax.figure.colorbar(im, ax=ax, **cbar_kw)
+    cbar.ax.set_ylabel(cbarlabel, rotation=-90, va="bottom")
+    ax.set_xticks(np.arange(200, step=20))
+    ax.set_yticks(np.arange(200, step=20))
+    ax.set_xticklabels(np.round(np.arange(-1, 1, step = .2), 2))
+    ax.set_yticklabels(np.round(np.arange(-1, 1, step = .2), 2))
+    plt.show()
+heat_map2()
