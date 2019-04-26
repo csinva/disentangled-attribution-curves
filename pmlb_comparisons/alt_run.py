0e5095a46ffebbb964473d154debc2326f849cc2
Chandan Singh <chandan_singh@berkeley.edu>
2019-04-25 22:03:23 -0700

    submit in par

---
+++ pmlb_comparisons/alt_run.py
@@ -0,0 +1,154 @@
+import sys
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pmlb as dsets
+import numpy as np
+import pickle as pkl
+from os.path import join as oj
+import os
+from copy import deepcopy
+import pandas as pd
+from numpy import array as arr
+
+# sklearn models
+sys.path.append('../scores')
+import scores
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+
+import interactions
+from scipy import interpolate
+
+dset_num = 0
+if len(sys.argv) > 1: # takes one arg (the dset_num)
+    dset_num = int(sys.argv[1])
+data_dir = '/scratch/users/vision/data/pmlb'
+out_dir = '/scratch/users/vision/chandan/pmlb'
+random_state = 42 # for each train_test_split
+
+results = pd.DataFrame(pkl.load(open(oj(out_dir, 'classification_results_orig_seeded.pkl'), 'rb')))
+os.makedirs(out_dir, exist_ok=True)
+
+def fit_altered(data_dir, out_dir, dset_num=0, random_state=42):
+
+    r = results
+    print('results shape', r.shape)
+
+
+    score_results = {
+            'feature_scores_mdi': [],
+    #         'feature_scores_mda': [],
+            'logit_score_orig_onevar': [],
+            'logit_score_altered_onevar': [],
+            'logit_score_altered_append': [],    
+            'variances': [],
+            'logit_score_altered_interaction_onevar': [],    
+            'logit_score_altered_interaction_append': [],
+        }
+
+#     for dset_num in tqdm(range(results.shape[0])): #tqdm(range(2)): #range(r.shape[0]):
+
+    # load data and rf
+    row = r.iloc[dset_num]    
+    dset_name = row.dset_name # results['dset_names'][idx_0] #dsets.classification_dataset_names[0]
+    X, y = dsets.fetch_data(dset_name, return_X_y=True, 
+                      local_cache_dir=data_dir)
+    train_X, test_X, train_y, test_y = train_test_split(X, y, random_state=random_state)
+    num_features = X.shape[1]
+    rf = row.rf
+    assert(rf.score(test_X, test_y) == row.rf_test_score) # check that acc matches
+
+
+    # feature importances
+    feature_scores_mdi = scores.get_importance_scores(rf, score_type='mdi', X=test_X, Y=test_y)
+    score_results['feature_scores_mdi'].append(feature_scores_mdi)
+    # feature_scores_mda = scores.get_importance_scores(rf, score_type='mda', X=test_X, Y=test_y)
+    # score_results['feature_scores_mda'].append(feature_scores_mda)
+
+
+    # get importance for 1d var
+    feat_num = np.argsort(feature_scores_mdi)[-1]
+    feat_vals = X[:, feat_num].reshape(-1, 1)
+    feat_vals_train = train_X[:, feat_num].reshape(-1, 1)
+    feat_vals_test = test_X[:, feat_num].reshape(-1, 1)
+    feat_val_min = np.min(feat_vals)
+    feat_val_max = np.max(feat_vals)
+
+    # appropriate variable to get importance for
+    S = np.zeros(num_features)
+    S[feat_num]= 1
+
+#     x_axis, scores_on_spaced_line = single_var_grid_scores_and_plot(rf, train_X, train_y, S, (feat_val_min, feat_val_max), plot=False)
+#     f = interpolate.interp1d(x_axis, scores_on_spaced_line, kind='nearest') # function to interpolate the scores
+    X_alt_train = interactions.interactions_forest(forest=rf, input_space_x=train_X, outcome_space_y=train_y, 
+                                             assignment=train_X, S=S, continuous_y=False).reshape(-1, 1)
+    X_alt_test = interactions.interactions_forest(forest=rf, input_space_x=train_X, outcome_space_y=train_y, 
+                                             assignment=test_X, S=S, continuous_y=False).reshape(-1, 1)
+
+
+
+    # fit only on one feature orig
+    logit = LogisticRegression(solver='liblinear', multi_class='auto', random_state=random_state) # liblinear best for small dsets, otherwise lbfgs
+    logit.fit(feat_vals_train, train_y)
+    score_results['logit_score_orig_onevar'].append(logit.score(feat_vals_test, test_y))
+
+    # fit only on one feature altered
+    logit = LogisticRegression(solver='liblinear', multi_class='auto', random_state=random_state) # liblinear best for small dsets, otherwise lbfgs
+    logit.fit(X_alt_train, train_y)
+    score_results['logit_score_altered_onevar'].append(logit.score(X_alt_test, test_y))
+
+    # fit with altered feature (appended)
+    logit = LogisticRegression(solver='liblinear', multi_class='auto', random_state=random_state) # liblinear best for small dsets, otherwise lbfgs
+    logit.fit(np.hstack((train_X, X_alt_train)), train_y)
+    score_results['logit_score_altered_append'].append(logit.score(np.hstack((test_X, X_alt_test)), test_y))    
+
+    # fit with 2D interaction (using feat_num and some other variable)
+    '''
+    variances = np.zeros(num_features) # variance of max feature with other features
+    for i in range(num_features):
+        if not i == feat_num:
+            S = np.zeros(num_features)
+            S[feat_num]= 1
+            S[i] = 1
+        variances[i] = interactions.variance2D(forest=rf, X=train_X, y=train_y, S=S, 
+                                               intervals='auto', dis='auto', continuous_y=False)
+    score_results['variances'].append(variances)
+    '''
+    score_results['variances'].append(np.nan)
+
+    # for feat with max interaction with the original max feat (chosen with mdi), refit using the interaction
+    # feat_num_2 = np.argmax(variances)
+    feat_num_2 = np.argsort(feature_scores_mdi)[-2]
+    S = np.zeros(num_features)
+    S[feat_num]= 1
+    S[feat_num_2] = 1
+
+    X_interaction_train = interactions.interactions_forest(forest=rf, input_space_x=train_X, outcome_space_y=train_y, 
+                                             assignment=train_X, S=S, continuous_y=False).reshape(-1, 1)
+    X_interaction_test = interactions.interactions_forest(forest=rf, input_space_x=train_X, outcome_space_y=train_y, 
+                                             assignment=test_X, S=S, continuous_y=False).reshape(-1, 1)
+
+
+    # fit only on one interaction altered
+    logit = LogisticRegression(solver='liblinear', multi_class='auto', random_state=random_state) # liblinear best for small dsets, otherwise lbfgs
+    logit.fit(X_interaction_train, train_y)
+    score_results['logit_score_altered_interaction_onevar'].append(logit.score(X_interaction_test, test_y))
+
+     # fit with altered interaction (appended)
+    logit = LogisticRegression(solver='liblinear', multi_class='auto', random_state=random_state) # liblinear best for small dsets, otherwise lbfgs
+    logit.fit(np.hstack((train_X, X_interaction_train)), train_y)
+    score_results['logit_score_altered_interaction_append'].append(logit.score(np.hstack((test_X, X_interaction_test)), test_y))    
+
+
+    # saving
+#     scores_df = pd.DataFrame(score_results)
+#     full_results = pd.concat([results.iloc[list(range(dset_num + 1))], scores_df], axis=1)
+    full_results = {**results.iloc[dset_num].to_dict(), **score_results}
+    out_str = f'single_full_results_{dset_num}_full'
+    pkl.dump(full_results, open(oj(out_dir, out_str + '.pkl'), 'wb'))
+
+fit_altered(data_dir, out_dir, dset_num=dset_num, random_state=42)
+print('success!')
---
+++ scripts/submit_pmlb.py
@@ -0,0 +1,16 @@
+import itertools
+from slurmpy import Slurm
+
+partition = 'high'
+
+# sweep different ways to initialize weights
+dset_nums = list(range(163)) # there are 163 total
+
+# run
+s = Slurm("pmlb", {"partition": partition, "time": "3-0"})
+
+# iterate
+for i in range(len(dset_nums)):
+    param_str = 'module load python; python3 /accounts/projects/vision/chandan/rf_interactions/pmlb_comparisons/alt_run.py '
+    param_str += str(dset_nums[i])
+    s.run(param_str)
