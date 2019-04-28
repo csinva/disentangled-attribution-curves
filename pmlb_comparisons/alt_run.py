4edc5b155ba246728f52508ac55373e30c45eae4
Chandan Singh <chandan_singh@berkeley.edu>
2019-04-28 14:54:19 -0700

    all scores in right range

--- pmlb_comparisons/alt_run.py
+++ pmlb_comparisons/alt_run.py
@@ -26,10 +26,10 @@ classification_only = True
 if len(sys.argv) > 1: # first arg (the dset_name)
     dset_name = str(sys.argv[1])
 if len(sys.argv) > 2: # second arg (classification or regression)
-    classification_only = str(sys.argv[1]) == 'classification'
+    classification_only = str(sys.argv[2]) == 'classification'
 
 data_dir = '/scratch/users/vision/data/pmlb'
-out_dir = '/scratch/users/vision/chandan/pmlb/regression_3'
+out_dir = '/scratch/users/vision/chandan/pmlb/classification_3'
 random_state = 42 # for each train_test_split
 
 def get_lin(classification_only, random_state):
@@ -66,8 +66,6 @@ def fit_altered(data_dir, out_dir, dset_name, classification_only=True, random_s
     logit = get_lin(classification_only, random_state)
 
     # load data and rf
-#     row = r.iloc[dset_num]    
-#      = row.dset_name # results['dset_names'][idx_0] #dsets.classification_dataset_names[0]
     X, y = dsets.fetch_data(dset_name, return_X_y=True, 
                       local_cache_dir=data_dir)
     train_X, test_X, train_y, test_y = train_test_split(X, y, random_state=random_state)
@@ -80,8 +78,6 @@ def fit_altered(data_dir, out_dir, dset_name, classification_only=True, random_s
     
     num_features = X.shape[1]
     score_results['rf'].append(rf)
-#     rf = row.rf
-#     assert(rf.score(test_X, test_y) == row.rf_test_score) # check that acc matches
 
 
     # feature importances
@@ -102,9 +98,6 @@ def fit_altered(data_dir, out_dir, dset_name, classification_only=True, random_s
     # appropriate variable to get importance for
     S = np.zeros(num_features)
     S[feat_num]= 1
-
-#     x_axis, scores_on_spaced_line = single_var_grid_scores_and_plot(rf, train_X, train_y, S, (feat_val_min, feat_val_max), plot=False)
-#     f = interpolate.interp1d(x_axis, scores_on_spaced_line, kind='nearest') # function to interpolate the scores
     X_alt_train = interactions.interactions_forest(forest=rf, input_space_x=train_X, outcome_space_y=train_y, 
                                              assignment=train_X, S=S, continuous_y=continuous_y).reshape(-1, 1)
     X_alt_test = interactions.interactions_forest(forest=rf, input_space_x=train_X, outcome_space_y=train_y, 
@@ -156,7 +149,7 @@ def fit_altered(data_dir, out_dir, dset_name, classification_only=True, random_s
 
     # fit only on one interaction altered
     logit = get_lin(classification_only, random_state)
-    print('\tprerror', dset_name, 'shapes', X_interaction_train.shape, X_interaction_test.shape, train_y.shape, test_y.shape)
+    print('\t', dset_name, 'shapes', X_interaction_train.shape, X_interaction_test.shape, train_y.shape, test_y.shape)
     try:
         logit.fit(X_interaction_train, train_y)
         score_results['logit_score_altered_interaction_onevar'].append(logit.score(X_interaction_test, test_y))
@@ -171,10 +164,7 @@ def fit_altered(data_dir, out_dir, dset_name, classification_only=True, random_s
 
 
     # saving
-#     scores_df = pd.DataFrame(score_results)
-#     full_results = pd.concat([results.iloc[list(range(dset_num + 1))], scores_df], axis=1)
     os.makedirs(out_dir, exist_ok=True)
-#     full_results = {**results.iloc[dset_num].to_dict(), **score_results}
     out_str = f'{dset_name}_full'
     pkl.dump(score_results, open(oj(out_dir, out_str + '.pkl'), 'wb'))
 
